aa5d678f34a4903109a581021a74cce7cc2c977b
Thundervoltgh1 <eniolusi@gmail.com>
2024-12-21 14:57:23 +0000

    Add files via upload

---
+++ calender.py
@@ -0,0 +1,17 @@
+from tkinter import*
+import calendar
+root=Tk()
+root.title("Calender")
+root.geometry("400x400")
+root.config(background="Blue")
+a=Label(root,text="")
+a.grid(row=1,column=1)
+r=Label(root,text="CALENDER",bg="white",fg="Blue",font=("Ariel",50,'bold'))
+r.grid(row=2,column=2)
+e=Entry(root,bg="Grey",fg="white")
+e.grid(row=3,column=2)
+s=Button(root,text="Show",fg="green",bg='white')
+s.grid(row=4,column=2)
+d=Button(root,text="exit",fg="red",bg='white',command=exit)
+d.grid(row=5,column=2)
+root.mainloop()
