fe84c142badd31c6d6738a293c55974faee75b41
Thundervoltgh1 <eniolusi@gmail.com>
2025-01-04 14:26:29 +0000

    Update calender.py

--- calender.py
+++ calender.py
@@ -10,8 +10,21 @@ r=Label(root,text="CALENDER",bg="white",fg="Blue",font=("Ariel",50,'bold'))
 r.grid(row=2,column=2)
 e=Entry(root,bg="Grey",fg="white")
 e.grid(row=3,column=2)
-s=Button(root,text="Show",fg="green",bg='white')
+def show_calender():
+    newtk=Tk()
+    newtk.config(background="White")
+    newtk.title("Calender")
+    newtk.geometry("600x600")
+    cal=int(e.get())
+    cal1=calendar.calendar(cal)
+    h=Label(newtk,text=cal1,font="Consolas 10 bold")
+    h.grid(row=5,column=1,padx=20)
+
+    newtk.mainloop()
+s=Button(root,text="Show",fg="green",bg='white',command=show_calender)  
 s.grid(row=4,column=2)
 d=Button(root,text="exit",fg="red",bg='white',command=exit)
 d.grid(row=5,column=2)
+
+
 root.mainloop()
